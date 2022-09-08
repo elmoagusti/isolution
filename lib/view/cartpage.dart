0b17d9d8022ff66d8d79d5609cc79b884c176094
elmoagusti <elmoagusti.ea@gmail.com>
2022-09-08 10:35:42 +0700

    ubah money

--- lib/view/cartpage.dart
+++ lib/view/cartpage.dart
@@ -128,7 +128,7 @@ class CartPage extends StatelessWidget {
                           ),
                         ),
                         onPressed: () {
-                          message(context, "Please Input Money", false);
+                          message(context, "Please Input ", false);
 
                           //fake process data to APi
                           Timer(const Duration(seconds: 10), () {
